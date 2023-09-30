b2023502e85552454002b941d3d8c77688abf0cc
Aditya Hegde <adityahegde712@gmail.com>
2023-09-30 16:49:24 +0530

    Added follow and incorrect parse table generation, will fix later

--- Activity 4/CFG_reader.py
+++ Activity 4/CFG_reader.py
@@ -32,39 +32,74 @@ def get_firsts(cfg):
     return dict(zip(non_terminals, firsts))
 
 
+def bad_follow(bad, cfg):
+    pass
+
+
 def get_follows(cfg, firsts, non_terminals):
     productions = []
-    follows = []
+    follows = {}
     # Get productions
     for line in cfg:
         productions.append(line[3:])
     
     # Compute follows
     for non_terminal in non_terminals: # Find the follow for each non-terminal
-        follow = []
+        follows[non_terminal] = []
 
         # Add $ to follow of first non-terminal
         if non_terminal == non_terminals[0]:
-            follow.append('$')
+            follows[non_terminal].append('$')
         
         # Main follow computation
-        for production in productions: # Iterate through each production
+        for production, line in zip(productions, cfg): # Iterate through each production
             if non_terminal in production: 
                 if not non_terminal == production[-1]: # If the non-terminal is not the last character in the production
                     if production[production.index(non_terminal) + 1].islower():
-                        follow.append(production[production.index(non_terminal) + 1])
+                        follows[non_terminal].append(production[production.index(non_terminal) + 1])
                     else:
-                        follow.append(firsts[production[production.index(non_terminal) + 1]])
-                elif not non_terminal == production[0]: # If this is the production for a non-terminal that is not the current non-terminal
-                    pass # Save this for your rubber duck programming moment
-                # You are at the point where you're checking for the follows of the non-terminal when it's last, and the current production does not belong to the current non-terminal
-                # THE FLAW IN THIS CODE IS THAT THE PRODUCTION DOES NOT CONTAIN THE RESPECTIVE NON-TERMINAL
-                # I WILL NEED TO CHANGE IT FROM ITERATING THROUGH THEM SEPARATELY TO ITERATING THROUGH THE LINES OF THE CFG ITSELF WITHOUT SPLITTING IT
-                # AAAAAAAAAAAAAAAAAAAAAAA EUREKA BUT I NEED TO SLEEP
-                # I WILL CONTINUE TOMORROW
-                # I WILL CONTINUE TOMORROW
-                # I WILL CONTINUE TOMORROW
-                # GOOD JOB ME
+                        follows[non_terminal].append(firsts[production[production.index(non_terminal) + 1]])
+                elif not non_terminal == line[0]: # If this is the production for a non-terminal that is not the current non-terminal
+                    follows[non_terminal] += follows[line[0]] # Save this for your rubber duck programming moment
+                else: 
+                    pass # If the non-terminal is the last character in its own production, do nothing
+
+    return follows
+
+
+def construct_parsing_table(grammar, firsts, follows, non_terminals):
+    parsing_table = {}
+
+    # Initialize the parsing table
+    for non_terminal in non_terminals:
+        parsing_table[non_terminal] = {}
+
+    print(parsing_table)
+
+    for production in grammar:
+        left, right = production.split('->')
+        right = right.strip()
+        first_of_right = first_of_string(right, firsts)
+
+        for symbol in first_of_right:
+            if symbol != 'ε':
+                parsing_table[left][symbol] = production
+
+        if 'ε' in first_of_right:
+            for follow_symbol in follows[left]:
+                parsing_table[left][follow_symbol] = production
+
+    return parsing_table
+
+
+def first_of_string(s, firsts):
+    first_set = set()
+    for symbol in s:
+        if symbol in firsts.keys():
+            first_set.update(firsts[symbol])
+            if 'ε' not in firsts[symbol]:
+                break
+    return first_set
 
 
 if __name__ == '__main__':
@@ -76,4 +111,8 @@ if __name__ == '__main__':
 
     print(f"Split CFG: \n{cfg}")
     print(f"\nFirsts: \n{firsts}")
-    print(f"\nFollows: \n{follows}")
+    print(f"\nFollows: \n{follows}\n")
+
+
+    parsing_table = construct_parsing_table(cfg, firsts, follows, non_terminals)
+    print(parsing_table)
